60308775554defeb0f2cce358c98013eabb75f72
Edward Abboud <97112418+EdwardAbboud@users.noreply.github.com>
2022-04-05 19:36:42 +0200

    Update homeView.js

--- src/views/homeView.js
+++ src/views/homeView.js
@@ -3,7 +3,7 @@ function createHomeView() {
   const root = document.createElement("div");
   root.innerHTML = String.raw`
     <div class="welcome-img-container">
-    <img class="welcome-img" src="../public/assets/show-me.png">
+    <img class="welcome-img" src="./public/assets/show-me.png">
     </div>
     <a class='button' href="#showList" id="get-show-button">Get show!</a>
     <div class="banner-container">
